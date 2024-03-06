7a8a8b469cde55a65592fccfe36b8b3060af9ab6
Sridhar-Konidela <sridharkonidela@outlook.com>
2024-03-06 12:52:29 +0530

    imported react and capitalised function in error page

--- src/app/error.jsx
+++ src/app/error.jsx
@@ -1,8 +1,9 @@
 'use client';
 
 import { useEffect } from "react";
+import React from "react";
 
-export default function error({error,reset}) {
+export default function Error({error,reset}) {
     useEffect(()=>{
         console.log(error);
     },[error]);
