1d49264d0f84c7ebcef071a91b17c7e1ac20c004
Sridhar-Konidela <sridharkonidela@outlook.com>
2024-03-06 11:54:20 +0530

    Added suspense element to navabr

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import NavbarItem from './NavbarItem'
 
 export default function Navbar() {
   return (
-    <div className='flex dark:bg-amber-600 bg-amber-400 p-4 lg:text-xl justify-center gap-6  '>
-        <NavbarItem title='Trending' param='fetchTrending'/>
-        <NavbarItem title='Top Rated' param='fetchTopRated'/>
-    </div>
+    <Suspense fallback={<>Loading...</>}>
+        <div className='flex dark:bg-amber-600 bg-amber-400 p-4 lg:text-xl justify-center gap-6  '>
+            <NavbarItem title='Trending' param='fetchTrending'/>
+            <NavbarItem title='Top Rated' param='fetchTopRated'/>
+        </div>
+    </Suspense>
   )
 }
